b212e6314e09d2930e7b56b8ea0e77df42919465
Vincent Gonzalez <vincentmpgonzalez@gmail.com>
2022-10-10 22:08:29 -0400

    Update appointment post handler

--- src/main.go
+++ src/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -10,6 +13,23 @@ import (
 	"github.com/julienschmidt/httprouter"
 	_ "github.com/mattn/go-sqlite3"
 )
+
+var database *sql.DB
+
+func checkIfAppointmentTimeIsAvailable(trainer_id int, start_time string, ends_at string) bool {
+	var appointment Appointment
+	err := database.QueryRow(`SELECT * FROM appointments
+	WHERE trainer_id = ? AND starts_at = ? AND ends_at = ?`,
+	 trainer_id, start_time, ends_at).Scan(&appointment)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return true
+		}
+		return false // some other error
+	}
+
+	return false // an appointment matching the criteria was found
+}
 
 func indexHandler(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
 	if req.URL.Path != "/" {
@@ -28,6 +48,32 @@ func appointmentsHandler(res http.ResponseWriter, req *http.Request, params http
 }
 
 func appointmentsPostHandler(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	requestBody, err := ioutil.ReadAll(io.LimitReader(req.Body, 1048576))
+	if err != nil {
+		// write error response res.
+	}
+	var appointment Appointment
+	if err = json.Unmarshal(requestBody, appointment); err != nil {
+		// write error response
+	}
+
+	if isAvailable := checkIfAppointmentTimeIsAvailable(
+		appointment.TrainerId, appointment.StartsAt, appointment.EndsAt); !isAvailable {
+		// return bad request
+	}
+
+	// save request into DB
+	insertSQL := `INSERT INTO appointments(trainer_id, user_id, starts_at, ends_at) VALUES (?, ?, ?, ?)`
+	statement, err := database.Prepare(insertSQL)
+	if err != nil {
+		// 500 error
+	}
+	_, err = statement.Exec(appointment.TrainerId, appointment.UserId, appointment.StartsAt, appointment.EndsAt)
+	if err != nil {
+		// 500 error
+	}
+	res.WriteHeader(200)
+
 	res.Write([]byte("<h1>Post successful!</h1>"))
 }
 
