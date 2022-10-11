c9f6c8bdb39ff27e010cfb41c063fb3a2b6895dc
Vincent Gonzalez <vincentmpgonzalez@gmail.com>
2022-10-11 18:26:19 -0400

    Remove appointment time fields

--- src/appointment.go
+++ src/appointment.go
@@ -1,8 +1,8 @@
 package main
 
 type Appointment struct {
-	TrainerId string `json:"trainer_id"`
-	UserId string `json:"user_id"`
-	StartsAt string `json:"starts_at"`
-	EndsAt string `json:"ends_at"`
+	Id uint `json:"id"`
+	TrainerId uint `json:"trainer_id"`
+	UserId uint `json:"user_id"`
+	AppointmentTime
 }
