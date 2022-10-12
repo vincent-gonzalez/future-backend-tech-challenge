b801712ac09ad134472800b6f667ee04bfc115c4
Vincent Gonzalez <vincentmpgonzalez@gmail.com>
2022-10-12 18:43:19 -0400

    Move utils into own package

---
+++ src/utils/utils.go
@@ -0,0 +1,31 @@
+package utils
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+	"github.com/vincent-gonzalez/future-backend-homework-project/src/types"
+)
+
+const Datetime_format_without_t = "2006-01-02 15:04:05-07:00"
+const Datetime_format = "2006-01-02T15:04:05-07:00"
+
+func ConvertDateTimeToRFC3339(dateTime string, currentFormat string) string {
+	temp, _ := time.Parse(currentFormat, dateTime)
+	return temp.Format(time.RFC3339)
+}
+
+func writeResponse(w http.ResponseWriter, responseBody interface{}, responseCode int) {
+	w.WriteHeader(responseCode)
+	json.NewEncoder(w).Encode(responseBody)
+}
+
+func WriteDataResponse(w http.ResponseWriter, status string, data interface{}, responseCode int) {
+	dataResponse := types.CreateDataResponse(status, data)
+	writeResponse(w, dataResponse, responseCode)
+}
+
+func WriteErrorResponse(w http.ResponseWriter, errorMessage string, errorCode int) {
+		errorResponse := types.CreateErrorResponse(errorMessage)
+		writeResponse(w, errorResponse, errorCode)
+}
